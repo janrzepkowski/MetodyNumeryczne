9a97e385629cc0cff9e735bac24acbb099f9717d
247773 <247773@edu.p.lodz.pl>
2024-03-16 01:59:38 +0100

    small fn

--- Zadanie1/metody.py
+++ Zadanie1/metody.py
@@ -46,6 +46,11 @@ def bisekcja(wybor, poczatek, koniec, iteracje, eps):
             return x0
 
 
+def sieczna_krok(wybor_p, poczatek_p, koniec_p):
+    return koniec_p - (wartosc(wybor_p, koniec_p) * (koniec_p - poczatek_p)) / (
+            wartosc(wybor_p, koniec_p) - wartosc(wybor_p, poczatek_p))
+
+
 def siecznych(wybor, poczatek, koniec, iteracje, eps):
     x0 = 0
 
@@ -56,8 +61,8 @@ def siecznych(wybor, poczatek, koniec, iteracje, eps):
         if iteracje == 0:  # d - osiągnięcie zadanej dokładności obliczeń
             n = 1
             while abs(koniec - poczatek) >= eps:
-                x0 = koniec - (wartosc(wybor, koniec) * (koniec - poczatek)) / (
-                            wartosc(wybor, koniec) - wartosc(wybor, poczatek))
+                x0 = sieczna_krok(wybor, poczatek, koniec)
+
                 if wartosc(wybor, x0) == 0:
                     print("Korzystajac z metody siecznych udalo sie znalezc dokladne rozwiazanie (ε = 0), po "
                           + str(n + 1) + " iteracjach")
@@ -72,8 +77,8 @@ def siecznych(wybor, poczatek, koniec, iteracje, eps):
 
         else:  # i - wykonanie określonej przez użytkownika liczby iteracji
             for n in range(iteracje):
-                x0 = koniec - (wartosc(wybor, koniec) * (koniec - poczatek)) / (
-                            wartosc(wybor, koniec) - wartosc(wybor, poczatek))
+                x0 = sieczna_krok(wybor, poczatek, koniec)
+
                 if wartosc(wybor, x0) == 0:
                     print("Korzystajac z metody siecznych udalo sie znalezc dokladne rozwiazanie (ε = 0), po "
                           + str(n + 1) + " iteracjach")
